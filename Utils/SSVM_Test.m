dfabf1d14126168a3ab5743046d8f6e6cef399b5
Alberto Carlevaro <99175531+AlbiCarle@users.noreply.github.com>
2024-05-09 15:14:50 -0700

    Update and rename SSVM_Test.m to MCSVM_Test.m

--- Utils/SSVM_Test.m
+++
@@ -1,6 +0,0 @@
-function y = SSVM_Test(Xtr, Ytr, Xts, alpha_bar, b, b_eps, kernel, param, eta)
-
-K = KernelMatrix(Xtr,Xts,kernel,param);
-D = diag(Ytr);
-
-y = -sign(-b -eta*(K'*(D*alpha_bar)) + b_eps); 
